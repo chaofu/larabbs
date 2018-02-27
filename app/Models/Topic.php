01a9ebb51d6e9cee3d736561e25580f181f9f86b
chaofu <chaofu123456@163.com>
2018-02-27 11:25:00 +0000

    topics seed

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,6 @@ namespace App\Models;
 
 class Topic extends Model
 {
+    protected $table = 'topics';
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 }
