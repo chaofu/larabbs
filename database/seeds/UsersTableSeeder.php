f8fc5f6be2f8e682b244c5dc41e415b141557b7a
chaofu <chaofu123456@163.com>
2018-03-04 11:16:10 +0000

    Topic index page

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
-
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -13,7 +12,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //获取实例
+        //
+        // 获取 Faker 实例
         $faker = app(Faker\Generator::class);
 
         // 头像假数据
@@ -26,28 +26,28 @@ class UsersTableSeeder extends Seeder
             'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
         ];
 
-        //生成集合数据
-
+        // 生成数据集合
         $users = factory(User::class)
-                                    ->times(10)
-                                    ->make()
-                                    ->each(function ($user, $index)   use ($faker, $avatars)
-                                    {
-                                        // 从头像数组中随机取出一个并赋值
-                                        $user->avatar = $faker->randomElement($avatars);
-                                    }
-                                    );
-        //让隐藏字段可以见
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index)
+            use ($faker, $avatars)
+            {
+                // 从头像数组中随机取出一个并赋值
+                $user->avatar = $faker->randomElement($avatars);
+            });
         // 让隐藏字段可见，并将数据集合转换为数组
         $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
 
-        //插入数据库中
+        // 插入到数据库中
         User::insert($user_array);
+
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'chaofu';
-        $user->email = 'chaofu@yousails.com';
+        $user->name = 'Summer';
+        $user->email = 'summer@yousails.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+
     }
 }
