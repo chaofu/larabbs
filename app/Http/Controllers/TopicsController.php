5089b422c568f463bc76ecf291b8bb346903708c
chaofu <chaofu123456@163.com>
2018-03-05 14:39:11 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,8 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
+
 
 class TopicsController extends Controller
 {
@@ -14,9 +16,10 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
-		$topics = Topic::with('user','category')->paginate(30);
+		$topics = $topic->withOrder($request->order)
+                         ->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
